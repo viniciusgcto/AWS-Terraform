f0bd8ebd48c25865f2c564990c5c885f688f5619
viniciusgcto <viniciusgcto@gmail.com>
2022-05-09 13:19:13 -0300

    commit inicial

---
+++ job_spark/job_spark.py
@@ -0,0 +1,9 @@
+# arquivo simples para treino de upload no Terraform
+
+usuario = input('Insira seu login: ')
+
+def printar(user):
+    
+    print(f"Nome: {user}")
+
+printar(usuario)
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "base_bucket_name" {
+    default = "datalake-igti-edc-tf-treino"
+  
+}
+
+variable "ambiente" {
+    default = "producao"
+  
+}
+
+variable "numero_conta" {
+    default = 343554143164
+  
+}
